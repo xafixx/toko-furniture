dde2da49cb1b6ddeb0c7ef3df6e433905391d171
Fikri <m.fikriaqsha@gmail.com>
2021-07-22 18:33:28 +0700

    adding some files

---
+++ README.md
@@ -0,0 +1,23 @@
+========================
+PROJECT TOKO FURNITURE
+========================
+
+Project dibuat menggunakan Apache Netbeans IDE 12.3 Java/JDK 16
+
+Hal-hal yang perlu dilakukan sebelum membuka/menjalankan project
+pada Apache Netbeans IDE:
+   * Tambahkan library
+       Library yang dibutuhkan disini adalah Mysql-connector,
+       file Mysql-connector berada di \Project_TokoFurniture\dist\lib\ 
+       file berekstensi .jar.
+       
+       untuk menambahkan libraries: 
+        - buka project pada Apache Netbeans klik kanan libraries,
+          klik ADD JAR/Folder, pilih file mysql-connector-java-8.0.25.jar
+          yang berada di folder \Project_TokoFurniture\dist\lib\
+
+Jika ingin menjalankan jar file tanpa IDE:
+    - Export database tokofurnituredb.sql
+    - Buka cmd menuju ke \Project_TokoFurniture\dist\
+    - Terdapat file Project_TokoFurniture.jar
+    - jalankan dengan perintah java -jarProject_TokoFurniture.jar [ENTER] 
---
+++ database/tokofurnituredb.sql
@@ -0,0 +1,80 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 22, 2021 at 01:07 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tokofurnituredb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `user` varchar(8) DEFAULT NULL,
+  `pass` varchar(8) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`user`, `pass`) VALUES
+('admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `barang`
+--
+
+CREATE TABLE `barang` (
+  `id_barang` int(11) NOT NULL,
+  `nama_barang` varchar(255) DEFAULT NULL,
+  `harga_barang` int(11) DEFAULT NULL,
+  `stok` int(11) DEFAULT NULL,
+  `deskripsi` varchar(255) DEFAULT NULL,
+  `photo` mediumblob DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id_barang` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
